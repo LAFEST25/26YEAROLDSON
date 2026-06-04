779e9000bf0f4d4814ca820a7ec233d789e1af3a
LAFEST25 <lfest50@gmail.com>
2026-06-04 20:04:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -758,7 +758,7 @@ document.body.appendChild(titleEl);
 
 const registerBtn = document.createElement('a');
 
-registerBtn.href = 'registration.html';
+registerBtn.href = 'https://lafest2026registration.lovable.app/';
 registerBtn.target = '_blank';
 
 registerBtn.textContent = 'REGISTER NOW';
